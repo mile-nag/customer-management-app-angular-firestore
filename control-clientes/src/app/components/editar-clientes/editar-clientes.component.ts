5dbcaa0be0dad506232427ff047f867fc1ad7f67
Milena Genez <genezmilena@gmail.com>
2025-03-01 12:57:31 -0300

    Agrega estructura de componentes e instala Firebase
    - Crea la carpeta components con los siguientes componentes:
      - clientes
      - footer
      - header
      - login
      - editar-clientes
      - no-encontrado
      - tablero
    - Instala dependencias: irebase y @angular/fire.

---
+++ control-clientes/src/app/components/editar-clientes/editar-clientes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-clientes',
+  imports: [],
+  templateUrl: './editar-clientes.component.html',
+  styleUrl: './editar-clientes.component.css'
+})
+export class EditarClientesComponent {
+
+}
