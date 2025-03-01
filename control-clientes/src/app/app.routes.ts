aff85ac0c50cf0226d55f0fe823f672c2f8a116a
Milena Genez <genezmilena@gmail.com>
2025-03-01 13:52:03 -0300

    Configura rutas básicas de la aplicación

--- control-clientes/src/app/app.routes.ts
+++ control-clientes/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { NoEncontradoComponent } from './components/no-encontrado/no-encontrado.component';
+import { EditarClientesComponent } from './components/editar-clientes/editar-clientes.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: TableroComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cliente/editar/:id', component: EditarClientesComponent},
+  {path: '**', component: NoEncontradoComponent}
+
+];
