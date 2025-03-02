a74cdbad5d0a268bf46c67220f829fe44160a476
Milena Genez <genezmilena@gmail.com>
2025-03-02 13:58:42 -0300

    Configura Firebase y entornos de desarrollo en la aplicación

---
+++ control-clientes/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA0z3Jj2_92HZC2iqS3AuOX2hBrkukfdOg',
+    authDomain: 'control-clientes-ff08f.firebaseapp.com',
+    projectId: 'control-clientes-ff08f',
+    storageBucket: 'control-clientes-ff08f.firebasestorage.app',
+    messagingSenderId: '91103654298',
+    appId: '1:91103654298:web:60dba559f5cb841083ca23',
+  },
+};
